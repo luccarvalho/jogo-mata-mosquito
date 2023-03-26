1130384a5ef3f1d673047b56a139cf108d1967c7
Lucas <lucasalexandre_82@hotmail.com>
2023-03-26 12:59:54 -0300

    Criando a lógica para deixar o personagem mosquito aparecendo de forma dinâmica e aleatória na tela.

--- js/script.js
+++ js/script.js
@@ -6,43 +6,67 @@ function ajustaTamanhoTela() {
     largura = window.innerWidth;
     altura = window.innerHeight;
 
-    console.log(largura, altura);
+    posicaoAleatoria();
 }
 
-ajustaTamanhoTela();
+// fazendo o mosquito aparecer na tela automaticamente a cada 1 segundo
+setInterval(function () { ajustaTamanhoTela() }, 1000);
 
-var posicaoX = Math.floor(Math.random() * largura) - 90;
-var posicaoY = Math.floor(Math.random() * altura) - 90;
 
-posicaoX = posicaoX < 0 ? 0 : posicaoX;
-posicaoY = posicaoY < 0 ? 0 : posicaoY;
+function posicaoAleatoria() {
 
-console.log(posicaoX, posicaoY);
+    // remove o mosquito anterior da tela
+    if (document.getElementById("mosquito")) {
+        document.getElementById("mosquito").remove();
+    }
 
-// criando o documento html
-var mosquito = document.createElement("img");
-mosquito.src = "img/mosca.png";
-mosquito.className = tamanhoAleatorio();
-mosquito.style.position = "absolute";
-mosquito.style.left = posicaoX + "px";
-mosquito.style.top = posicaoY + "px";
+    var posicaoX = Math.floor(Math.random() * largura) - 90;
+    var posicaoY = Math.floor(Math.random() * altura) - 90;
 
-document.body.appendChild(mosquito);
+    posicaoX = posicaoX < 0 ? 0 : posicaoX;
+    posicaoY = posicaoY < 0 ? 0 : posicaoY;
 
-// function tamanhoAleatorio() {
-//     var classe = Math.floor(Math.random() * 3);
+    console.log(posicaoX, posicaoY);
 
-//     switch (classe) {
-//         case 0:
-//             return "mosquito1";
+    // criando o elemento (mosquito) html de forma dinâmica
+    var mosquito = document.createElement("img");
+    mosquito.src = "img/mosca.png";
+    mosquito.className = tamanhoAleatorio() + " " + ladoAleatorio();
+    mosquito.style.position = "absolute";
+    mosquito.style.left = posicaoX + "px";
+    mosquito.style.top = posicaoY + "px";
+    mosquito.id = "mosquito";
 
-//         case 1:
-//             return "mosquito2";
+    document.body.appendChild(mosquito);
 
-//         case 2:
-//             return "mosquito3";
-//     }
-// }
+}
 
-// console.log(tamanhoAleatorio());
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3);
+
+    switch (classe) {
+        case 0:
+            return "mosquito1";
+
+        case 1:
+            return "mosquito2";
+
+        case 2:
+            return "mosquito3";
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2);
+
+    switch (classe) {
+        case 0:
+            return "ladoA";
+
+        case 1:
+            return "ladoB";
+
+    }
+}
+
 
