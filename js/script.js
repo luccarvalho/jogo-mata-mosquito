7b28b08c13c05f4ed9bef7c5ea747f5835ffb1f1
Lucas <lucasalexandre_82@hotmail.com>
2023-03-29 13:15:56 -0300

    Criando a tela inicial do jogo e a lógica para redirecionamento em caso de vitória.

--- js/script.js
+++ js/script.js
@@ -1,90 +1,9 @@
-var largura = 0;
-var altura = 0;
+function iniciarJogo() {
 
-var vidas = 1;
+    var nivel = document.getElementById("nivel").value;
 
-// var tempo = 10;
-
-function ajustaTamanhoTela() {
-
-    largura = window.innerWidth;
-    altura = window.innerHeight;
-
-    posicaoAleatoria();
-}
-
-// fazendo o mosquito aparecer na tela automaticamente a cada 1 segundo
-setInterval(function () { ajustaTamanhoTela() }, 2000);
-
-
-function posicaoAleatoria() {
-
-    // remove o mosquito anterior da tela
-    if (document.getElementById("mosquito")) {
-        document.getElementById("mosquito").remove();
-
-        if (vidas > 3) {
-            window.location.href = "game_over.html";
-
-        } else {
-            // assim que o mosquito some da tela, o coração cheio é trocado pelo coração vazio
-            document.getElementById("v" + vidas).src = "img/coracao_vazio.png";
-
-            vidas++;
-        }
-    }
-
-    var posicaoX = Math.floor(Math.random() * largura) - 90;
-    var posicaoY = Math.floor(Math.random() * altura) - 90;
-
-    posicaoX = posicaoX < 0 ? 0 : posicaoX;
-    posicaoY = posicaoY < 0 ? 0 : posicaoY;
-
-
-    // criando o elemento (mosquito) html de forma dinâmica
-    var mosquito = document.createElement("img");
-    mosquito.src = "img/mosca.png";
-    mosquito.className = tamanhoAleatorio() + " " + ladoAleatorio();
-    mosquito.style.position = "absolute";
-    mosquito.style.left = posicaoX + "px";
-    mosquito.style.top = posicaoY + "px";
-    mosquito.id = "mosquito";
-    mosquito.onclick = function () {
-        this.remove();
-    }
-
-    document.body.appendChild(mosquito);
-
-}
-
-// faz o tamanho do mosquito ser alterado dinamicamente
-function tamanhoAleatorio() {
-    var classe = Math.floor(Math.random() * 3);
-
-    switch (classe) {
-        case 0:
-            return "mosquito1";
-
-        case 1:
-            return "mosquito2";
-
-        case 2:
-            return "mosquito3";
+    if (nivel === "") {
+        alert("Selecione um nível para iniciar o jogo");
+        return false;
     }
 }
-
-// faz o mosquito ter a imagem espelhada dinamicamente
-function ladoAleatorio() {
-    var classe = Math.floor(Math.random() * 2);
-
-    switch (classe) {
-        case 0:
-            return "ladoA";
-
-        case 1:
-            return "ladoB";
-
-    }
-}
-
-
